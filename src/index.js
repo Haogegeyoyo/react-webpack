00aff0412837291456e56000bf8c26af15c9c3d1
haogegeyoyo <1468853309@qq.com>
2022-03-30 21:25:24 +0800

    feat : 创建 redux 文件

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React,{Component} from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import App from './app';
 import './index.less'
 import { HashRouter, BrowserRouter } from 'react-router-dom'
+import store from "@/store/"
 // const Router = process.env.NODE_ENV === 'development' ? HashRouter : BrowserRouter;
 const Router = HashRouter 
 
@@ -10,9 +12,11 @@ const Router = HashRouter
 ReactDOM.render(
   // <React.StrictMode>
   //  需要路由监听的，统统放在 <Router> 组件下，<app>
-    <Router>
-      <App />
-    </Router>,
+  <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+  </Provider>,
   // </React.StrictMode>,
   document.getElementById('app')
 );
